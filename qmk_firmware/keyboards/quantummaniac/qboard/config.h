7eb91780edb9431ca97753171ae11060949628a9
Chamath Wijesekera <chamath2.wijeseekera@gmail.com>
2021-08-28 11:12:50 -0400

    Fix mismatched pin

--- qmk_firmware/keyboards/quantummaniac/qboard/config.h
+++ qmk_firmware/keyboards/quantummaniac/qboard/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                        00  01  02  03  04  05  06  07  08  09  10  11  12  13  14  15  16  17  18  19
  */
 #define MATRIX_ROW_PINS { A0, A1, A2, A3, A4, A5 }
-#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, D0, D1, D5, D6, C7, C6, C5, C4, C3, C2, C1, C0 }
+#define MATRIX_COL_PINS { B0, B1, B2, B3, B4, B5, B6, B7, D0, D1, D5, D7, C7, C6, C5, C4, C3, C2, C1, C0 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
