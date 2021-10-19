621d0d2fcead46361e5019be308989e39c090c1a
jlsajfj <jlsajfj@hotmail.com>
2021-10-18 22:53:55 -0400

    revert

--- qmk_firmware/keyboards/quantummaniac/qboard/config.h
+++ qmk_firmware/keyboards/quantummaniac/qboard/config.h
@@ -62,7 +62,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define BACKLIGHT_PIN D4
 #define BACKLIGHT_LEVELS 3
-#define BACKLIGHT_BREATHING
+// #define BACKLIGHT_BREATHING
 
 //#define RGB_DI_PIN E2
 //#ifdef RGB_DI_PIN
